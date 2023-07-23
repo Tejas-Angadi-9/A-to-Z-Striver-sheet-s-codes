c3b45669ec0b22a844c1461a4d98834b07ee33b1
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2023-07-23 16:26:04 +0530

    added 1st problem

---
+++ 3. Arrays/1. Easy/1.cpp
@@ -0,0 +1,24 @@
+// Find the largest element in an array
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int findMax(int arr[], int n)
+{
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (maxi < arr[i])
+            maxi = arr[i];
+    }
+    return maxi;
+}
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {8, 10, 5, 7, 9};
+
+    cout << findMax(arr, n) << endl;
+}
