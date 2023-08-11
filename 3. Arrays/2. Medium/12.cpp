d9ee495fd199444a274120aec8beedf165cc426d
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2023-08-11 14:29:42 +0530

    11 Aug'23

--- 3. Arrays/2. Medium/12.cpp
+++ 3. Arrays/2. Medium/12.cpp
@@ -7,12 +7,14 @@ using namespace std;
 void rotate(vector<vector<int>> &matrix){
     int n = matrix.size();
 
+    //! Transposing clockwise
     for(int i=0; i<n; i++){
         for(int j=0; j<i; j++){
             swap(matrix[i][j], matrix[j][i]);
         }
     }
 
+    //! Reversing each row
     for(int i=0; i<n; i++){
         reverse(matrix[i].begin(), matrix[i].end());
     }
